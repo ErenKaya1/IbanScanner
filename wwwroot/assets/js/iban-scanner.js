6ecd111db6601511c5f2c6c5b8a02fb247085eca
ErenKaya1 <wtcerberus@gmail.com>
2021-01-29 17:41:07 +0300

    added client side validation for reset password form

--- wwwroot/assets/js/iban-scanner.js
+++ wwwroot/assets/js/iban-scanner.js
@@ -94,4 +94,35 @@ $(document).ready(function () {
       });
     }
   });
+
+  // validation for reset password form
+  $(".resetpassword-form").on("submit", function (e) {
+    if (
+      $("input[name='Password']").val() == "" ||
+      $("input[name='PasswordConfirm']").val() == ""
+    ) {
+      e.preventDefault();
+      toastr["error"]("Please fill in the required fields.", "", {
+        positionClass: "toast-top-right",
+        closeButton: true,
+        progressBar: true,
+        preventDuplicates: true,
+        newestOnTop: true,
+        rtl: $("body").attr("dir") === "rtl" || $("html").attr("dir") === "rtl",
+      });
+    } else if (
+      $("input[name='Password']").val() !=
+      $("input[name='PasswordConfirm']").val()
+    ) {
+      e.preventDefault();
+      toastr["error"]("Passwords must match.", "", {
+        positionClass: "toast-top-right",
+        closeButton: true,
+        progressBar: true,
+        preventDuplicates: true,
+        newestOnTop: true,
+        rtl: $("body").attr("dir") === "rtl" || $("html").attr("dir") === "rtl",
+      });
+    }
+  });
 });
